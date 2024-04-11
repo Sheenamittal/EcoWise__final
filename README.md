1a4389bf748bb34a0be0818b788c60751e9feb95
manasvi-tech <manasviarora28@gmail.com>
2024-04-11 20:41:19 +0530

    final

---
+++ README.md
@@ -0,0 +1,46 @@
+# EcoWise
+
+EcoWise is a web application that allows users to disseminate critical information related to environmental issues and champion innovative strategies for mitigating their impact. Users can also navigate event calenders.
+
+## Getting Started
+
+### Prerequisites
+
+_Node.js_ - Download and install Node.js from https://nodejs.org/en/download/
+
+_MongoDB_ - Download and install MongoDB from https://www.mongodb.com/download-center/community
+
+### Installing
+
+1. Clone the repository
+
+```
+git clone https://github.com/manasvi-tech/Ecowise.git
+```
+
+2. Install dependencies
+
+```
+npm install
+```
+
+3. Start the server
+
+```
+npm start
+```
+
+4. Open http://localhost:5502 in your browser
+
+## Built With
+
+- [Node.js](https://nodejs.org/en/) - JavaScript runtime
+- [Express](https://expressjs.com/) - Web framework
+- [MongoDB](https://www.mongodb.com/) - Database
+- [Mongoose](https://mongoosejs.com/) - MongoDB object modeling tool
+- [Passport](http://www.passportjs.org/) - Authentication middleware
+- [Bootstrap](https://getbootstrap.com/) - Front-end component library
+- [HBS](https://handlebarsjs.com/) - Templating engine
+
+
+
---
+++ backend/public/javascript/chart1.js
@@ -0,0 +1,56 @@
+const ctx = document.getElementById('barchart');
+
+  const barChart=new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Air', 'Water', 'Soil'],
+      datasets: [{
+        label: 'Pollution',
+        data: [91, 80, 33],
+        backgroundColor: [
+            'rgba(144, 238, 144, 0.2)',
+            'rgba(0, 128, 0, 0.2)',
+            'rgba(173, 255, 47, 0.2)',
+          ],
+          borderColor: [
+            'rgba(144, 238, 144, 1)',
+            'rgba(0, 128, 0, 1)',
+            'rgba(173, 255, 47, 1)',
+          ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+        indexAxis: 'y',
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  ctx.onclick = function(e) {
+    const activeElement = barChart.getElementsAtEventForMode(e, 'nearest', { intersect: true });
+
+    if (activeElement.length > 0) {
+        const dataIndex = activeElement[0].index;
+
+        
+        barChart.data.datasets[0].backgroundColor[dataIndex] = 'rgba(255, 0, 0, 0.2)';
+        barChart.update();
+
+      
+        if (dataIndex === 0) {
+            window.location.href = 'https://www.who.int/health-topics/air-pollution#tab=tab_1';
+        } else if (dataIndex === 1) {
+            window.location.href = 'https://www.who.int/news-room/fact-sheets/detail/drinking-water';
+        } else if (dataIndex === 2) {
+            window.location.href = 'https://www.fao.org/documents/card/en?details=cb4827en';
+        }
+    }
+};
